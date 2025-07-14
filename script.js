adeb786c5fe1eb99a1430bcc9b4c5151f6442919
boben195 <boben195@ukr.net>
2025-07-14 12:50:44 +0300

    add some style

--- script.js
+++ script.js
@@ -3,26 +3,40 @@ const btn = document.getElementById("btn");
 const output = document.getElementById("output-code");
 const selectedShift = document.querySelector('input[name="step"]')
 const btnDecrypt = document.getElementById("btn-decrypt")
+const langSelect = document.getElementById("lang-select")
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const alphabetUk = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я']
+
 
 function caesarEncrypt(text, step) {
   return text.split('').map(el => {
-    if (!alphabet.includes(el)) return el;
-
-    const index = alphabet.indexOf(el);
-    const shiftedIndex = (index + step) % alphabet.length;
-    return alphabet[shiftedIndex];
+    if (!alphabet.includes(el) && !alphabetUk.includes(el)) return el;
+    if (langSelect.value === "en") {
+       const index = alphabet.indexOf(el);
+       const shiftedIndex = (index + step) % alphabet.length;
+       return alphabet[shiftedIndex];
+    } else {
+      const index = alphabetUk.indexOf(el);
+       const shiftedIndex = (index + step) % alphabetUk.length;
+       return alphabetUk[shiftedIndex];
+    }
   }).join('');
 }
 
 function ceasarDecrypt(text, step) {
   return text.split('').map(el => {
-    if (!alphabet.includes(el)) return el
+    if (!alphabet.includes(el) && !alphabetUk.includes(el)) return el;
 
-    const index = alphabet.indexOf(el);
-    const shiftedIndex = (index - step) % alphabet.length;
-    return alphabet[shiftedIndex];
+    if (langSelect.value === "en") {
+      const index = alphabet.indexOf(el);
+      const shiftedIndex = (index - step + alphabet.length) % alphabet.length;
+      return alphabet[shiftedIndex];
+    } else {
+      const index = alphabetUk.indexOf(el);
+      const shiftedIndex = (index - step + alphabetUk.length) % alphabetUk.length;
+      return alphabetUk[shiftedIndex];
+    }
   }).join('');
 }
 
